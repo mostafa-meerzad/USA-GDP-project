eac00bdcc95ba5e2cdd5ba3b1c85126c459aeead
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-08-07 18:03:46 +0430

    complete the view and functionalities of the challenge

--- main.js
+++ main.js
@@ -16,13 +16,22 @@ fetch(url)
     // console.log(gdpData)
     const xScale = d3
       .scaleLinear()
+
       .domain([0, gdpData.length])
+      .range([padding, width - padding]);
+
+    const xDataScale = d3
+      .scaleLinear()
+      .domain([
+        d3.min(gdpData, (d) => d[0].split("-")[0]),
+        d3.max(gdpData, (d) => d[0].split("-")[0]),
+      ])
       .range([padding, width - padding]);
 
     const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(gdpData, (d) => d[1])])
-      .range([padding, height - padding]);
+      .range([height - padding, padding]);
 
     svg
       .selectAll("rect")
@@ -30,9 +39,50 @@ fetch(url)
       .enter()
       .append("rect")
       .attr("x", (d, i) => xScale(i))
-      .attr("y", (d) => height - yScale(d[1]))
+      .attr("y", (d) => yScale(d[1]))
       .attr("width", 1)
-      .attr("height", (d) => yScale(d[1]) - padding)
-      .attr("class", "rect");
+      .attr("height", (d) => height - yScale(d[1]) - padding)
+      .attr("class", "bar")
+      .attr("data-date", (d) => d[0])
+      .attr("data-gdp", (d) => d[1])
+      .append("title")
+      .attr("id", "tooltip")
+      .text((d) => `date: ${d[0]} gdp: $${d[1]}`);
+    // .attr("data-date", (d) => d[0])
+    // .attr("data-gdp", (d) => d[1]);
 
+    const xAxis = d3.axisBottom(xDataScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    // xAxis.ticks(10).tickValues([1,155,275])
+    // xAxis.tickValues(gdpData.map((d) => {
+    // return Number(d[0].split("-")[0])
+    // }))
+    // xAxis.tickValues([
+    //   1947, 1947, 1947, 1947, 1948, 1948, 1948, 1948, 1949, 1949, 1949, 1949,
+    //   1950, 1950, 1950, 1950, 1951, 1951, 1951, 1951, 1952, 1952, 1952, 1952,
+    //   1953, 1953, 1953, 1953, 1954, 1954, 1954, 1954, 1955, 1955, 1955, 1955,
+    //   1956, 1956, 1956, 1956, 1957, 1957, 1957, 1957, 1958, 1958, 1958, 1958,
+    //   1959, 1959, 1959, 1959, 1960, 1960, 1960, 1960, 1961, 1961, 1961, 1961,
+    //   1962, 1962, 1962, 1962, 1963, 1963, 1963, 1963, 1964, 1964, 1964, 1964,
+    //   1965, 1965, 1965, 1965, 1966, 1966, 1966, 1966, 1967, 1967, 1967, 1967,
+    //   1968, 1968, 1968, 1968, 1969, 1969, 1969, 1969, 1970, 1970, 1970, 1970,
+    //   1971, 1971, 1971, 1971,
+    // ]);
+    // .tickArguments(100)
+
+    // xAxis.tickValues([100, 400, 200, 600]);
+
+    // console.log(gdpData.map((d) => Number(d[0].split("-")[0])));
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - padding})`)
+      .attr("id", "x-axis")
+      .call(xAxis);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(${padding} , 0)`)
+      .attr("id", "y-axis")
+      .call(yAxis);
   });
